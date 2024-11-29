58298eebc2613445f0bcad47922a070522cd4ac0
monsm <imonsm@qq.com>
2024-11-29 17:38:42 +0800

    Update install.sh

--- filebrowser/install.sh
+++ filebrowser/install.sh
@@ -1,94 +1,52 @@
 #!/bin/sh
+
 source /koolshare/scripts/base.sh
 alias echo_date='echo 【$(TZ=UTC-8 date -R +%Y年%m月%d日\ %X)】:'
-MODEL=
-FW_TYPE_CODE=
-FW_TYPE_NAME=
+module=filebrowser
 DIR=$(cd $(dirname $0); pwd)
-module=${DIR##*/}
+ROG_86U=0
+EXT_NU=$(nvram get extendno)
+EXT_NU=${EXT_NU%_*}
+odmpid=$(nvram get odmpid)
+productid=$(nvram get productid)
+[ -n "${odmpid}" ] && MODEL="${odmpid}" || MODEL="${productid}"
+LINUX_VER=$(uname -r|awk -F"." '{print $1$2}')
 
-get_model(){
-	local ODMPID=$(nvram get odmpid)
-	local PRODUCTID=$(nvram get productid)
-	if [ -n "${ODMPID}" ];then
-		MODEL="${ODMPID}"
-	else
-		MODEL="${PRODUCTID}"
-	fi
-}
-
-get_fw_type() {
-	local KS_TAG=$(nvram get extendno|grep -Eo "kool.+")
-	if [ -d "/koolshare" ];then
-		if [ -n "${KS_TAG}" ];then
-			FW_TYPE_CODE="2"
-			FW_TYPE_NAME="${KS_TAG}官改固件"
-		else
-			FW_TYPE_CODE="4"
-			FW_TYPE_NAME="koolshare梅林改版固件"
-		fi
-	else
-		if [ "$(uname -o|grep Merlin)" ];then
-			FW_TYPE_CODE="3"
-			FW_TYPE_NAME="梅林原版固件"
-		else
-			FW_TYPE_CODE="1"
-			FW_TYPE_NAME="华硕官方固件"
-		fi
-	fi
-}
-
-platform_test(){
-	local LINUX_VER=$(uname -r|awk -F"." '{print $1$2}')
-	local ARCH=$(uname -m)
-	if [ -d "/koolshare" -a -f "/usr/bin/skipd" -a "${LINUX_VER}" -ge "41" ];then
-		if [ "${ARCH}" != "aarch64" ];then
-			exit_install 2
-		else
-			echo_date 机型："${MODEL} ${FW_TYPE_NAME} 符合安装要求，开始安装插件！"
-		fi
-	else
-		exit_install 1
-	fi
-}
-
-set_skin(){
-	local UI_TYPE=ASUSWRT
-	local SC_SKIN=$(nvram get sc_skin)
-	local ROG_FLAG=$(grep -o "680516" /www/form_style.css|head -n1)
-	local TUF_FLAG=$(grep -o "D0982C" /www/form_style.css|head -n1)
-	if [ -n "${ROG_FLAG}" ];then
-		UI_TYPE="ROG"
-	fi
-	if [ -n "${TUF_FLAG}" ];then
-		UI_TYPE="TUF"
-	fi
-	
-	if [ -z "${SC_SKIN}" -o "${SC_SKIN}" != "${UI_TYPE}" ];then
-		echo_date "安装${UI_TYPE}皮肤！"
-		nvram set sc_skin="${UI_TYPE}"
-		nvram commit
-	fi
+# 获取固件类型
+_get_type() {
+  local FWTYPE=$(nvram get extendno|grep koolshare)
+  if [ -d "/koolshare" ];then
+    if [ -n $FWTYPE ];then
+      echo "koolshare官改固件"
+    else
+      echo "koolshare梅林改版固件"
+    fi
+  else
+    if [ "$(uname -o|grep Merlin)" ];then
+      echo "梅林原版固件"
+    else
+      echo "华硕官方固件"
+    fi
+  fi
 }
 
 exit_install(){
-	local state=$1
-	case $state in
-		1)
-			echo_date "本插件适用于【koolshare 梅林改/官改 hnd/axhnd/axhnd.675x】固件平台！"
-			echo_date "你的固件平台不能安装！！!"
-			echo_date "本插件支持机型/平台：https://github.com/koolshare/rogsoft#rogsoft"
-			echo_date "退出安装！"
-			rm -rf /tmp/filebrowser* >/dev/null 2>&1
-			exit 1
-			;;
-		0|*)
-			rm -rf /tmp/filebrowser* >/dev/null 2>&1
-			exit 0
-			;;
-	esac
+  local state=$1
+  case $state in
+    1)
+      echo_date "本插件适用于【koolshare 梅林改/官改 hnd/axhnd/axhnd.675x armsoft 384】固件平台！"
+      echo_date "你的固件平台不能安装！！!"
+      echo_date "如有问题请联系我们反馈相关信息 http://forum.filebrowser.com"
+      echo_date "退出安装！"
+      rm -rf /tmp/${module}* >/dev/null 2>&1
+      exit 1
+      ;;
+    0|*)
+      rm -rf /tmp/${module}* >/dev/null 2>&1
+      exit 0
+      ;;
+  esac
 }
-
 dbus_nset(){
 	# set key when value not exist
 	local ret=$(dbus get $1)
@@ -96,7 +54,6 @@ dbus_nset(){
 		dbus set $1=$2
 	fi
 }
-
 install_now() {
 	# default value
 	local TITLE="FileBrowser"
@@ -185,10 +142,15 @@ install_now() {
 	exit_install
 }
 
-install() {
-  get_model
-  get_fw_type
+if [ "`uname -o|grep Merlin`" ] && [ -d "/koolshare" ] && [ -n "`nvram get buildno|grep -E 384\|386`" ];then
+  echo_date 固件平台【koolshare merlin armv7l 384/386】符合安装要求，开始安装插件！
+elif [ -d "/koolshare" -a -f "/usr/bin/skipd" -a "${LINUX_VER}" -ge "41" ];then
+  # 判断路由架构和平台：koolshare固件，并且linux版本大于等于4.1
+  echo_date 机型：${MODEL} $(_get_type) 符合安装要求，开始安装插件！
   install_now
-}
-
-install
+else
+  exit_install 1
+fi
+# 完成
+echo_date "filebrowser 插件安装完毕！"
+exit_install
